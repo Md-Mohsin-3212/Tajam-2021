f122ac0e76865d7b592926364fbe625d32176dda
Md Mohsin <86037115+Md-Mohsin-3212@users.noreply.github.com>
2021-07-07 14:07:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -223,4 +223,27 @@ $(document).ready(function () {
     },
   };
   scrolltotop.init();
+   // Smooths scrolling
+  $("body").scrollspy({ target: ".navbar", offset: 50 });
+  //animation scroll js
+  var html_body = $("html, body");
+  $("nav a").on("click", function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        html_body.animate(
+          {
+            scrollTop: target.offset().top - 50,
+          },
+          1500
+        );
+        return false;
+      }
+    }
+  });
 });
